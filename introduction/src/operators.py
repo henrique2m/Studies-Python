28b02fab0c25a6fed0cc046d54da0a6b5513c5cf
Henrique Moreira <btdavintehenrique@gmail.com>
2020-05-19 14:01:01 -0300

    Arithmetic operators

---
+++ introduction/src/operators.py
@@ -0,0 +1,37 @@
+print('------------------Saída de dados--------------------')
+
+print('Hello World!') # Quebra de linha é equivalente ao ";", finalizando um comando;
+
+print('-----------Operadores aritiméticos---------------------')
+
+print(1+2) # adição
+
+print(2-1) # subtração
+
+print(2*6) # multiplicação
+
+print(6/2) # divisão
+
+print(2**3) # exponenciação
+
+print(4%2) # módulo 
+
+print('--------------Operadores de concatenação------------')
+
+print('Henrique '+ 'Moreira') # Concatena tipos de dados iguais
+
+print('Henrique', 1.71, 60.00) # Concatena tipos de dados diferêntes 
+
+print('-----------------Entrada de dados ------------------')
+
+name = input('Qual é o seu nome jovem padawan?')
+
+print(name, ' é um nome bonito! (:')
+
+# exercício 1
+
+n1 = int(input('Informe um número.'))
+n2 = int(input('Informe outro número.'))
+
+print(n1+n2)
+ 
